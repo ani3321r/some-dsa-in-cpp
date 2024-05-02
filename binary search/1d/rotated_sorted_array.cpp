82e9d6f5066e9a0ee28ef484c5cb50a5274127fe
ani3321r <ani3321r@gmail.com>
2024-05-02 23:45:12 +0530

    rotated sorted array

---
+++ binary search/1d/rotated_sorted_array.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int searchPos(vector<int> &a, int k){
+    int n = a.size();
+    int low=0, high=n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(a[mid] == k) return mid;
+        if(a[low] <= a[mid]){
+            if(a[low] <= k && k <= a[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        else{
+            if(a[mid] <= k && k <= a[high]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+void init_code(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main(){
+    init_code();
+    int num, target;
+    cin >> num;
+    cin >> target;
+    vector<int> arr(num);
+    for(int i=0; i<num; i++) cin >> arr[i];
+    cout << searchPos(arr, target);
+    return 0;
+}
