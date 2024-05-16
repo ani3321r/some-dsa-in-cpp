33e79df79e5b7481b773e4ce6c94cdc48949a0c3
ani3321r <ani3321r@gmail.com>
2024-05-16 20:45:52 +0530

    combination sum 2

---
+++ recursion/subsequences patterns/combination_sum_2.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
+  if (target == 0) {
+    ans.push_back(ds);
+    return;
+  }
+  for (int i = ind; i < arr.size(); i++) {
+    if (i > ind && arr[i] == arr[i - 1]) continue;
+    if (arr[i] > target) break;
+    ds.push_back(arr[i]);
+    findCombination(i + 1, target - arr[i], arr, ans, ds);
+    ds.pop_back();
+  }
+}
+
+vector < vector < int >> combinationSum2(vector < int > & candidates, int target) {
+  sort(candidates.begin(), candidates.end());
+  vector < vector < int >> ans;
+  vector < int > ds;
+  findCombination(0, target, candidates, ans, ds);
+  return ans;
+}
+
+void init_code(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main(){
+    init_code();
+    int num, target;
+    cin >> num;
+    cin >> target;
+    vector<int> arr(num);
+    for(int i=0; i<num; i++) cin >> arr[i];
+
+    vector<vector<int>> ans = combinationSum2(arr, target);
+
+    cout << "all the possible combinations are:\n";
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "\n";
+    }
+}
