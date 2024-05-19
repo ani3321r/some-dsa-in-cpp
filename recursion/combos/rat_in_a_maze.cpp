0bc3860c1dc8360d0335d409c6413ea050801fc8
ani3321r <ani3321r@gmail.com>
2024-05-19 18:55:58 +0530

    rat in a maze

---
+++ recursion/combos/rat_in_a_maze.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int i, int j, vector<vector<int>> &a, int n, vector<string> &ans, string move, vector<vector<int>> &vis, int di[], int dj[]){
+    
+    if(i == n-1 && j == n-1){
+        ans.push_back(move);
+        return;
+    }
+
+    string dir = "DLRU";
+    for (int ind = 0; ind < 4; ind++) {
+      int nexti = i + di[ind];
+      int nextj = j + dj[ind];
+      if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && !vis[nexti][nextj] && a[nexti][nextj] == 1) {
+        vis[i][j] = 1;
+        solve(nexti, nextj, a, n, ans, move + dir[ind], vis, di, dj);
+        vis[i][j] = 0;
+      }
+    }
+    
+}
+
+vector<string> findPath(vector<vector<int>> &m, int n){
+    vector<string> ans;
+    vector<vector<int>> vis(n, vector<int> (n,0));
+    int di[] = {+1, 0, 0, -1};
+    int dj[] = {0, -1, 1, 0};
+    if(m[0][0] == 1) solve(0, 0, m, n, ans, "", vis, di, dj);
+    return ans;
+}
+
+int main(){
+    vector<vector<int>> m = {{1,1,0,0},{1,1,0,1},{1,1,0,1},{0,1,1,1}};
+    int n = m.size();
+    vector<string> ans = findPath(m,n);
+    if(ans.size() == 0) cout << -1;
+    else{
+        for(int i=0; i<ans.size(); i++){
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+    }
+    
+}
+
+
+/*
+with if statements
+
+//down
+    if(i+1<n && !vis[i+1][j] && a[i+1][j] == 1){
+        vis[i][j] = 1;
+        solve(i+1, j, a, n, ans, move + 'D', vis);
+        vis[i][j] = 0;
+    }
+
+    //left
+    if(j-1>=0 && !vis[i][j-1] && a[i][j-1] == 1){
+        vis[i][j] = 1;
+        solve(i, j-1, a, n, ans, move + 'L', vis);
+        vis[i][j] = 0;
+    }
+
+    //right
+    if(j+1>=0 && !vis[i][j+1] && a[i][j+1] == 1){
+        vis[i][j] = 1;
+        solve(i, j+1, a, n, ans, move + 'R', vis);
+        vis[i][j] = 0;
+    }
+
+    //up
+    if(i-1<n && !vis[i-1][j] && a[i-1][j] == 1){
+        vis[i][j] = 1;
+        solve(i-1, j, a, n, ans, move + 'U', vis);
+        vis[i][j] = 0;
+    }
+
+
+*/
