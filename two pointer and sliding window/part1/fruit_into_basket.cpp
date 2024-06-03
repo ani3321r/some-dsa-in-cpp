9af0f28d29041fffa8e7241317ba00c3660d1364
ani3321r <ani3321r@gmail.com>
2024-06-03 16:39:33 +0530

    fruits into basket

---
+++ two pointer and sliding window/part1/fruit_into_basket.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxFruits(vector<int> &nums) {
+    int l = 0, r = 0, maxFruit = 0;
+    map<int, int> mpp;
+    while (r < nums.size()) {
+        mpp[nums[r]]++;
+        if (mpp.size() > 2) {
+            while (mpp.size() > 2) {
+                mpp[nums[l]]--;
+                if (mpp[nums[l]] == 0) mpp.erase(nums[l]);
+                l++;
+            }
+        }
+        if (mpp.size() <= 2) {
+            maxFruit = max(maxFruit, r - l + 1);
+        }
+        r++;
+    }
+    return maxFruit;
+}
+
+void init_code(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}    
+
+int main(){
+    init_code();
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i=0; i<n; i++) cin >> nums[i];
+    cout << maxFruits(nums) << endl;
+}
