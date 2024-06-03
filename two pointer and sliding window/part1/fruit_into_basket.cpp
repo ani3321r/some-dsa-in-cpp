032dad03a08c1748ecd25154b30fe40ae222d6a0
ani3321r <ani3321r@gmail.com>
2024-06-03 23:18:38 +0530

    a more optimised code for fruit into basket

--- two pointer and sliding window/part1/fruit_into_basket.cpp
+++ two pointer and sliding window/part1/fruit_into_basket.cpp
@@ -36,3 +36,26 @@ int main(){
     for(int i=0; i<n; i++) cin >> nums[i];
     cout << maxFruits(nums) << endl;
 }
+
+//more optimised code
+//tc O(n)
+//sc O(1)
+/*
+int maxFruits(vector<int> &nums) {
+    int l = 0, r = 0, maxFruit = 0;
+    map<int, int> mpp;
+    while (r < nums.size()) {
+        mpp[nums[r]]++;
+        if (mpp.size() > 2) {
+            mpp[nums[l]]--;  
+            if (mpp[nums[l]] == 0) mpp.erase(nums[l]);
+            l++;
+        }
+        if (mpp.size() <= 2) {
+            maxFruit = max(maxFruit, r - l + 1);
+        }
+        r++;
+    }
+    return maxFruit;
+}
+*/
