9bc3514460a7c28ddfd2f8d61ece947192c34646
ani3321r <ani3321r@gmail.com>
2024-06-11 02:09:51 +0530

    iterative traversals

---
+++ binary tree/traversals/iterative_inorder.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+vector<int> iteIn(TreeNode* root) {
+    stack<TreeNode*> st;
+    TreeNode* node = root;
+    vector<int> ans;
+    while (node != NULL || !st.empty()) {
+        while (node != NULL) {
+            st.push(node);
+            node = node->left;
+        }
+        node = st.top();
+        st.pop();
+        ans.push_back(node->val);
+        node = node->right;
+    }
+    return ans;
+}
+
+TreeNode* buildTree(vector<string>& nodes) {
+    if (nodes.empty()) return NULL;
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->left = new TreeNode(stoi(nodes[i]));
+            q.push(node->left);
+        }
+        i++;
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->right = new TreeNode(stoi(nodes[i]));
+            q.push(node->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+void freeTree(TreeNode* root) {
+    if (root) {
+        freeTree(root->left);
+        freeTree(root->right);
+        delete root;
+    }
+}
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string node;
+    vector<string> nodes;
+    while (getline(ss, node, ' ')) {
+        nodes.push_back(node);
+    }
+    TreeNode* root = buildTree(nodes);
+    vector<int> result = iteIn(root);
+    cout << "Iterative Inorder Traversal:" << endl;
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+    freeTree(root);
+    return 0;
+}
