1349c9e3db998560b50a4f964417f4b9cfc719e7
ani3321r <ani3321r@gmail.com>
2024-06-16 15:21:51 +0530

    bottom view of a binary tree

---
+++ binary tree/basic problems/bottom_view.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+vector<int> bottomView(TreeNode *root){
+    vector<int> ans;
+    if(root == NULL) return ans;
+    map<int, int> mpp;
+    queue<pair<TreeNode*, int>> q;
+    q.push({root, 0});
+    while(!q.empty()){
+        auto it = q.front();
+        q.pop();
+        TreeNode* node = it.first;
+        int line = it.second;
+        mpp[line] = node->val;
+        if(node->left != NULL){
+            q.push({node->left, line-1});
+        }
+        if(node->right != NULL){
+            q.push({node->right, line+1});
+        }
+    }
+    for(auto it: mpp){
+        ans.push_back(it.second);
+    }
+    return ans;
+}
+
+TreeNode* buildTree(vector<string>& nodes) {
+    if (nodes.empty()) return NULL;
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->left = new TreeNode(stoi(nodes[i]));
+            q.push(node->left);
+        }
+        i++;
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->right = new TreeNode(stoi(nodes[i]));
+            q.push(node->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+void freeTree(TreeNode* root) {
+    if (root) {
+        freeTree(root->left);
+        freeTree(root->right);
+        delete root;
+    }
+}
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string node;
+    vector<string> nodes;
+    while (getline(ss, node, ' ')) {
+        nodes.push_back(node);
+    }
+    TreeNode* root = buildTree(nodes);
+    vector<int> ans = bottomView(root);
+    for(int i=0; i<ans.size(); i++) cout << ans[i] << " ";
+    freeTree(root);
+    return 0;
+}
