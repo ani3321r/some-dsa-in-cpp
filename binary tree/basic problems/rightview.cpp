11933acfd608bf82ee8476ca94f5aa48f0c5d66d
ani3321r <ani3321r@gmail.com>
2024-06-16 16:54:14 +0530

    right view of a binary tree

---
+++ binary tree/basic problems/rightview.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+void recursion(TreeNode *root, int level, vector<int> &res){
+    if(root == NULL) return;
+    if(res.size()==level) res.push_back(root->val);
+    recursion(root->right, level+1, res);
+    recursion(root->left, level+1, res);
+}
+
+vector<int> rightView(TreeNode *root){
+    vector<int> res;
+    recursion(root, 0, res);
+    return res;
+}
+
+TreeNode* buildTree(vector<string>& nodes) {
+    if (nodes.empty()) return NULL;
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->left = new TreeNode(stoi(nodes[i]));
+            q.push(node->left);
+        }
+        i++;
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->right = new TreeNode(stoi(nodes[i]));
+            q.push(node->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+void freeTree(TreeNode* root) {
+    if (root) {
+        freeTree(root->left);
+        freeTree(root->right);
+        delete root;
+    }
+}
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string node;
+    vector<string> nodes;
+    while (getline(ss, node, ' ')) {
+        nodes.push_back(node);
+    }
+    TreeNode* root = buildTree(nodes);
+    vector<int> ans = rightView(root);
+    for(int i=0; i<ans.size(); i++) cout << ans[i] << " ";
+    freeTree(root);
+    return 0;
+}
