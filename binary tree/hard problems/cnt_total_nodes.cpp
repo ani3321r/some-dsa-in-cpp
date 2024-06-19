f10ced454c93da3697f7bd7de8b0c437f2dd4500
ani3321r <ani3321r@gmail.com>
2024-06-19 15:08:28 +0530

    count total number of nodes

---
+++ binary tree/hard problems/cnt_total_nodes.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+int findLEHeight(TreeNode* node){
+    int height = 0;
+    while(node) {
+        height++;
+        node = node->left;
+    }
+    return height;
+}
+
+int findREHeight(TreeNode* node){
+    int height = 0;
+    while(node) {
+        height++;
+        node = node->right;
+    }
+    return height;
+}
+
+int cntNodes(TreeNode* root){
+    if(root == NULL)  return 0;
+    int lh = findLEHeight(root);
+    int rh = findREHeight(root);
+    if(lh == rh) return (1<<lh) - 1;
+
+    return 1 + cntNodes(root->left) + cntNodes(root->right); 
+}
+
+TreeNode* buildTree(vector<string>& nodes) {
+    if (nodes.empty()) return NULL;
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->left = new TreeNode(stoi(nodes[i]));
+            q.push(node->left);
+        }
+        i++;
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->right = new TreeNode(stoi(nodes[i]));
+            q.push(node->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+void freeTree(TreeNode* root) {
+    if (root) {
+        freeTree(root->left);
+        freeTree(root->right);
+        delete root;
+    }
+}
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string node;
+    vector<string> nodes;
+    while (getline(ss, node, ' ')) {
+        nodes.push_back(node);
+    }
+    TreeNode* root = buildTree(nodes);
+    cout << cntNodes(root);
+    freeTree(root);
+    return 0;
+}
