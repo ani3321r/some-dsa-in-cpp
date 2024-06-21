e8bdaaa34a334a9ad537bfafe516c6f66fee3666
ani3321r <ani3321r@gmail.com>
2024-06-21 18:15:19 +0530

    flatten a binary tree to a linked list

---
+++ binary tree/hard problems/flatten_to_linked_list.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *findPredicate(TreeNode *curr){
+    curr=curr->left;
+    while(curr->right!=NULL){
+        curr=curr->right;
+    }
+    return curr;
+}
+
+void flatten(TreeNode *root){
+    TreeNode *curr=root;
+    while(curr!=NULL){
+        if(curr->left){
+            TreeNode *pred=findPredicate(curr);
+            pred->right=curr->right;
+            curr->right=curr->left;
+            curr->left=NULL;
+        }   
+    curr=curr->right;
+    }
+    return;
+}
+
+TreeNode* buildTree(vector<string>& nodes) {
+    if (nodes.empty()) return NULL;
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->left = new TreeNode(stoi(nodes[i]));
+            q.push(node->left);
+        }
+        i++;
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->right = new TreeNode(stoi(nodes[i]));
+            q.push(node->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+void freeTree(TreeNode* root) {
+    if (root) {
+        freeTree(root->left);
+        freeTree(root->right);
+        delete root;
+    }
+}
+
+void printTree(TreeNode* root) {
+    if (root == NULL) return;
+    printTree(root->left);
+    cout << root->val << " ";
+    printTree(root->right);
+}
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string node;
+    vector<string> nodes;
+    while (getline(ss, node, ' ')) {
+        nodes.push_back(node);
+    }
+    TreeNode* root = buildTree(nodes);
+    flatten(root);
+    printTree(root);
+    freeTree(root);
+    return 0;
+}
