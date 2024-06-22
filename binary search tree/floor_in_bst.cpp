511284d9ad542b4483e50a92cdd96d3775717c59
ani3321r <ani3321r@gmail.com>
2024-06-22 22:19:51 +0530

    floor and ceil values of a bst

---
+++ binary search tree/floor_in_bst.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+int findFloor(TreeNode* root, int key){
+    int floor = -1;
+    while(root){
+        if(root->val == key){
+            floor = root->val;
+            return floor;
+        }
+        if(key > root->val){
+            floor = root->val;
+            root = root->right;
+        }
+        else{
+            root = root->left;
+        }
+    }
+    return floor;
+}
+
+TreeNode* buildTree(vector<string>& nodes) {
+    if (nodes.empty()) return NULL;
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->left = new TreeNode(stoi(nodes[i]));
+            q.push(node->left);
+        }
+        i++;
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->right = new TreeNode(stoi(nodes[i]));
+            q.push(node->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+void freeTree(TreeNode* root) {
+    if (root) {
+        freeTree(root->left);
+        freeTree(root->right);
+        delete root;
+    }
+}
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string node;
+    vector<string> nodes;
+    while (getline(ss, node, ' ')) {
+        nodes.push_back(node);
+    }
+    TreeNode* root = buildTree(nodes);
+    int k;
+    cin >> k;
+    cout << findFloor(root, k);
+    freeTree(root);
+    return 0;
+}
