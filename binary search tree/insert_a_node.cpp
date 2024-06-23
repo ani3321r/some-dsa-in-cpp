3162ceea74a6c625db13cd0b333604452db23b89
ani3321r <ani3321r@gmail.com>
2024-06-23 19:33:13 +0530

    insert a node in a bst

---
+++ binary search tree/insert_a_node.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* insertNode(TreeNode* root, int val){
+    if(root == NULL) return new TreeNode(val);
+    TreeNode *cur = root;
+    while(true){
+        if(cur->val <= val){
+            if(cur->right != NULL) cur = cur->right;
+            else{
+                cur->right = new TreeNode(val);
+                break;
+            }
+        } else{
+            if(cur->left != NULL) cur = cur->left;
+            else{
+                cur->left = new TreeNode(val);
+                break;
+            }
+        }
+    }
+    return root;
+}
+
+TreeNode* buildTree(vector<string>& nodes) {
+    if (nodes.empty()) return NULL;
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->left = new TreeNode(stoi(nodes[i]));
+            q.push(node->left);
+        }
+        i++;
+        if (i < nodes.size() && nodes[i] != "null") {
+            node->right = new TreeNode(stoi(nodes[i]));
+            q.push(node->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+void freeTree(TreeNode* root) {
+    if (root) {
+        freeTree(root->left);
+        freeTree(root->right);
+        delete root;
+    }
+}
+
+vector<int> printTree(TreeNode* root) {
+    vector<int> result;
+    if (root == nullptr) {
+        return result;
+    }
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            TreeNode* node = q.front();
+            q.pop();
+            result.push_back(node->val);
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+    }
+    return result;
+}
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string node;
+    vector<string> nodes;
+    while (getline(ss, node, ' ')) {
+        nodes.push_back(node);
+    }
+    TreeNode* root = buildTree(nodes);
+    int k;
+    cin >> k;
+    TreeNode* newRoot = insertNode(root, k);
+    vector<int> ans = printTree(newRoot);
+    for(int i=0; i<ans.size(); i++) cout << ans[i] << " ";
+    freeTree(root);
+    return 0;
+}
