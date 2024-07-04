e08d6ba2c5093a007943a6f859cfc0f0ce631a8f
ani3321r <ani3321r@gmail.com>
2024-07-04 17:17:33 +0530

    ditect cycles in a directed acyclic graph

---
+++ graphs/topo sort/ditect_cycles_of_DAG.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isCyclic(int V, vector<int> adj[]) {
+    vector<int> indegree(V, 0);
+    for(int i = 0; i < V; i++){
+        for(auto it : adj[i]){
+            indegree[it]++;
+        }
+    }
+    queue<int> q;
+    for(int i = 0; i < V; i++){
+        if(indegree[i] == 0){
+            q.push(i);
+        }
+    }
+    int cnt = 0;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        cnt++;
+        for(auto it : adj[node]){
+            indegree[it]--;
+            if(indegree[it] == 0) q.push(it);
+        }
+    }
+    if(cnt == V) return false;
+    return true;
+}
+
+void init_code() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    int V, E;
+    cin >> V >> E;
+    vector<int> adj[V];
+    for(int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        if(u >= 0 && u < V && v >= 0 && v < V) {
+            adj[u].push_back(v);
+        } else {
+            cout << "Invalid vertex!" << endl;
+            return 1;
+        }
+    }
+    cout << isCyclic(V, adj);
+    return 0;
+}
