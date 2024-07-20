3157f6478fb133a66f172104b9d8991a5ee0d0a9
ani3321r <ani3321r@gmail.com>
2024-07-20 21:13:58 +0530

    longest common subset

---
+++ dynamic programming/dp in strings/longest_common_sub.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lcs(string s, string t){
+    int n = s.size();
+    int m = t.size();
+    vector<int> prev(m+1, 0), cur(m+1, 0);
+    for(int j=0; j<=m; j++) prev[j] = 0;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(s[i-1] == t[j-1]) cur[j] = 1 + prev[j-1];
+            else cur[j] = max(prev[j], cur[j-1]);
+        }
+        prev = cur;
+    }
+    return prev[m];
+}
+
+void init_code() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+}
+
+int main() {
+    init_code();
+    string s, t;
+    cin >> s >> t;
+    
+    int result = lcs(s, t);
+    cout << result << endl;
+    
+    return 0;
+}
