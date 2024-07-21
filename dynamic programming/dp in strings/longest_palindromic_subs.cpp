9ee477b63f8fc24a1d7c3b2c5684ba4627981607
ani3321r <ani3321r@gmail.com>
2024-07-21 18:33:38 +0530

    lcs fixed

--- dynamic programming/dp in strings/longest_palindromic_subs.cpp
+++ dynamic programming/dp in strings/longest_palindromic_subs.cpp
@@ -3,22 +3,22 @@
 #include <algorithm>
 using namespace std;
 
-int lcs(string s, string t){
+int lcs(string s, string t) {
     int n = s.size();
     int m = t.size();
     vector<int> prev(m+1, 0), cur(m+1, 0);
-    int ans = 0;
     for(int i=1; i<=n; i++){
         for(int j=1; j<=m; j++){
             if(s[i-1] == t[j-1]){
                 cur[j] = 1 + prev[j-1];
-                ans = max(ans, cur[j]);
             } 
-            else cur[j] = 0;
+            else {
+                cur[j] = max(prev[j], cur[j-1]);
+            }
         }
         prev = cur;
     }
-    return ans;
+    return cur[m];
 }
 
 int longestPalin(string s){
