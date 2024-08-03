17fc7bc1802750a5bb83fa57785283eb7aaa700f
ani3321r <ani3321r@gmail.com>
2024-08-03 22:04:48 +0530

    length of cycle in a linked list

---
+++ linked list/medium ll/length_of_cycle.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *next;
+        Node(){
+            this->data = 0;
+            next = NULL;
+        }
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
+        Node(int data, Node* next){
+            this->data = data;
+            this->next = next;
+        }
+};
+
+Node *convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int findLength(Node* slow, Node* fast) {
+    int cnt = 1;
+    fast = fast->next;
+    while(slow != fast) {
+        cnt++;
+        fast = fast->next;
+    }
+    return cnt;
+}
+
+int lengthOfLoop(Node *head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            return findLength(slow, fast);
+        }
+    }
+    return 0;
+}
+
+void init_code() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+}
+
+int main(){
+    init_code();
+    int n;
+    cin >> n;
+    vector<int> ll(n);
+    for(int i=0; i<n; i++) cin >> ll[i];
+    Node* head = convertArr2LL(ll);
+
+    if (n > 1) {
+        Node* last = head;
+        while (last->next != NULL) {
+            last = last->next;
+        }
+        last->next = head->next;
+    }
+
+    int ans = lengthOfLoop(head);
+    cout << ans << endl;
+    return 0;
+}
